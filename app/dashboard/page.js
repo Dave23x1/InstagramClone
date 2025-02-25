d770a5cdd9330239ce1ecaeb29959ddd0338e704
Dave23x1 <ryandavedonos@gmail.com>
2025-02-25 16:46:38 +0800

    Updated

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,28 +2,27 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+
 export default function Dashboard() {
   const router = useRouter();
+  const session = useSession();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const checkAuth = async () => {
-      const response = await fetch("/api/checkAuth");
-      const data = await response.json();
-
-      if (!response.ok) {
-        router.push("/");
-      } else {
-        setLoading(false);
-      }
-    };
-
-    checkAuth();
-  }, [router]);
+    if (session.status === "unauthenticated") {
+      router.push("/");
+    } else if (session.status === "authenticated") {
+      setLoading(false);
+    }
+  }, [session.status, router]);
 
   if (loading) return <p>Loading...</p>;
 
-  return <section>
-    
-  </section>;
+  return (
+    <section>
+      {session?.data && <p>Welcome, {session.data.user.name}!</p>}
+      {/* Render menu bar */}
+    </section>
+  );
 }
--- pages/api/checkAuth.js
+++ pages/api/checkAuth.js
@@ -1,18 +1,22 @@
+import { getToken } from "next-auth/jwt";
 import { parse } from "cookie";
 import jwt from "jsonwebtoken";
 
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const cookies = parse(req.headers.cookie || "");
-  const token = cookies.authToken;
+  const authToken = cookies.authToken;
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  if (!token) {
+  if (!authToken && !session) {
     return res.status(401).json({ message: "Not authenticated" });
   }
 
   try {
-    jwt.verify(token, JWT_SECRET_KEY);
+    if (authToken) {
+      jwt.verify(authToken, JWT_SECRET_KEY);
+    }
     return res.status(200).json({ message: "Authenticated" });
   } catch (error) {
     return res.status(401).json({ message: "Invalid token" });
